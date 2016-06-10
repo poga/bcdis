98588b4b75f825e341d10cb1529b380c0bc92db0
Poga Po <poga.bahamut@gmail.com>
2016-06-10 21:31:56 +0800

    every account have an address

--- account.go
+++ account.go
@@ -5,6 +5,13 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
+	"encoding/asn1"
+	"math/big"
+
+	"github.com/tv42/base58"
+
+	"golang.org/x/crypto/ripemd160"
 )
 
 type Account struct {
@@ -13,6 +20,27 @@ type Account struct {
 
 func (a *Account) Public() *ecdsa.PublicKey {
 	return &a.Key.PublicKey
+}
+
+func (a *Account) Address() ([]byte, error) {
+	pub := ecdsaPublicKey{X: a.Public().X, Y: a.Public().Y}
+
+	serialized, err := asn1.Marshal(pub)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	hash := sha256.Sum256(serialized)
+	bytes := ripemd160.New().Sum(hash[:])
+
+	bigInt := new(big.Int).SetBytes(bytes)
+
+	return base58.EncodeBig([]byte{}, bigInt), nil
+}
+
+type ecdsaPublicKey struct {
+	X *big.Int
+	Y *big.Int
 }
 
 func NewAccount() (*Account, error) {
---
+++ account_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestAccount(t *testing.T) {
+	Convey("an account", t, func() {
+		a, err := NewAccount()
+		So(err, ShouldBeNil)
+
+		Convey("have private part and public part", func() {
+			// public key
+			So(a.Public(), ShouldNotBeNil)
+
+			// private key
+			So(a.Key, ShouldNotBeNil)
+		})
+
+		Convey("have an human readable address", func() {
+			address, err := a.Address()
+			So(err, ShouldBeNil)
+
+			So(string(address[:]), ShouldNotBeNil)
+		})
+
+	})
+}
